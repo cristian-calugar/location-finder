c2d8c31dbc3da7a7158be6870184a0456263a0fd
Cristian Calugar <cristian.calugar@rebeldot.com>
2018-11-09 11:04:41 +0200

    Initial commit

---
+++ app/src/main/java/com/rebeldot/locationfinder/SearchLocationsActivity.kt
@@ -0,0 +1,12 @@
+package com.rebeldot.locationfinder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchLocationsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_locations)
+    }
+}
